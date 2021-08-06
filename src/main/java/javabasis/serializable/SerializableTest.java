5faa16044ebfed9b6f2e4c338ee8ac055f638a82
Edwin-Xu <taoxu.xu@qunarservers.com>
2021-08-06 13:44:42 +0800

    re orgnized the project

---
+++ src/main/java/javabasis/serializable/SerializableTest.java
@@ -0,0 +1,11 @@
+package javabasis.serializable;
+
+/**
+ * @author taoxu.xu
+ * @date 8/6/2021 11:20 AM
+ */
+public class SerializableTest {
+    public static void main(String[] args) {
+        System.out.println("s");
+    }
+}
